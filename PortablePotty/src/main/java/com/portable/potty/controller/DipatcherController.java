5d3c034dd8255d0b924009c28d8b798e779a015e
HaroldMac <harold.macdonald52@gmail.com>
2018-12-26 21:12:50 -0700

    added select day option for dispatcher

--- PortablePotty/src/main/java/com/portable/potty/controller/DipatcherController.java
+++ PortablePotty/src/main/java/com/portable/potty/controller/DipatcherController.java
@@ -8,15 +8,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.portable.potty.model.Call;
 import com.portable.potty.model.Employee;
 import com.portable.potty.model.RunList;
+import com.portable.potty.model.Vehicle;
 import com.portable.potty.service.EmployeeService;
 import com.portable.potty.service.EmployeeServiceImpl;
 import com.portable.potty.service.RunListService;
 import com.portable.potty.service.RunListServiceImpl;
+import com.portable.potty.service.VehicleService;
+import com.portable.potty.service.VehicleServiceImpl;
 
 @Controller
 @SessionAttributes("employee")
@@ -39,20 +43,22 @@ public class DipatcherController {
 	
 	@RequestMapping(value="/dispatch/onShift", method = RequestMethod.GET)
 	public String dispatcherOnShift(Model model){
-		//Stuff to delete later
-		System.out.println("dispatch on shift controller called");
 		List<Employee> drivers = this.getDrivers();
 		model.addAttribute("drivers", this.printDriversAsHtmlTab(drivers));
 		model.addAttribute("runList", this.printAllRunListAsHTMl(drivers));
-
+		model.addAttribute("days", this.daysDropDownBox());		
 		return "dispatch/onShift";		
 	}
 	
 	@RequestMapping(value="/dispatch/onShift", method = RequestMethod.POST)
-	public String updateOnShift(){
-		//Stuff to delete later
-		System.out.println("dispatch on shift controller called");
-
+	public String updateOnShift(@RequestParam String selectedDay, Model model){
+		System.out.println("onshift post called");
+		List<Employee> drivers = this.getDrivers();
+		model.addAttribute("drivers", this.printDriversAsHtmlTab(drivers));
+		model.addAttribute("runList", this.printAllRunListAsHTMl(drivers));
+		model.addAttribute("days", this.daysDropDownBox());
+		System.out.println("the selected day is " + selectedDay);
+		
 		return "dispatch/onShift";		
 	}
 	
@@ -69,7 +75,6 @@ public class DipatcherController {
 	private List<Employee> getDrivers(){
 		List<Employee> employees = new ArrayList<Employee>();
 		EmployeeService es = new EmployeeServiceImpl();
-		//es = (EmployeeService) es.getAllEmployees();
 		for (Employee e: es.getAllEmployees()) {
 			if (e.getPosition().trim().toLowerCase().equals("driver")) {
 				employees.add(e);
@@ -90,9 +95,14 @@ public class DipatcherController {
 	
 	private String printDriversAsHtmlTab(List<Employee> employees) {
 		String html = "";
+		boolean firstEmployee = true;
 		for (Employee e: employees) {
-			
-			html += "<button class=\"tablinks\" onclick=\"openDriver(event, '" + e.getFirstName() + "')\">" + e.getFirstName() + "</button>";
+			if (firstEmployee) {
+				html += "<button class=\"tablinks\" onload=\"openDriver(event, '" + e.getFirstName() + "')\" onclick=\"openDriver(event, '" + e.getFirstName() + "')\">" + e.getFirstName() + "</button>";
+				firstEmployee = false;
+			} else {
+				html += "<button class=\"tablinks\" onclick=\"openDriver(event, '" + e.getFirstName() + "')\">" + e.getFirstName() + "</button>";
+			}
 		}
 		return html;
 	}
@@ -151,7 +161,6 @@ public class DipatcherController {
 		List<Call> calls = runList.getCalls();
 		for (Call call : calls) {
 			html += "<tr><td><img src='images/profilePicture.jpg' style = 'width:32px; height:32px; border:0;''></td><td><input type='checkbox'></td>";
-			System.out.println(call.getService());
 			html += "<td>" + call.getService() + "</td>";
 			html += "<td>" + call.getCustomer().getCustomerName() + "</td>";
 			html += "<td>" + call.getContactName() + "</td>";
@@ -159,8 +168,6 @@ public class DipatcherController {
 			html += "<td>" + this.getGoogleMapsLink(call.getLocation()) + "</td>";
 			if (call.getService().toLowerCase().equals("relocation")) {
 				html += "<td>" + this.getGoogleMapsLink(call.getRelocationAddress()) + "</td>";
-			} else {
-				System.out.println("No relocation" + " " + call.getService());
 			}
 			
 			html += "<td></td></tr>"; 
@@ -177,11 +184,20 @@ public class DipatcherController {
 	}
 	
 	private String getDayOfWeek(int day) {
-		System.out.println("the id of the day of the week is " + day);
 		String[] days = new String[] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thusday", "Friday", "Saturday" };
 		return days[day-1];
 	}
 
+	
+	private String daysDropDownBox() {
+		String dropDownBox = "<select name=\"selectedDay\" form=\"dayForm\">";
+		String[] days = new String[] { "Monday", "Tuesday", "Wednesday", "Thusday", "Friday", "Saturday", "Sunday"};
+		for (String day: days) {
+			dropDownBox += "<option value='" + day + "'>" + day + "</option>";
+		}
+		dropDownBox += "</select>";
+		return dropDownBox;
+	}
 	
 
 
